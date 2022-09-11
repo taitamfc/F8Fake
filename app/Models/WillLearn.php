d0ee7542909d526cb5efb1b7eecca859e473808b
unknown <haipro2141999@gmail.com>
2022-09-11 16:59:43 +0700

    model: course, level, will_learn

---
+++ app/Models/WillLearn.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WillLearn extends Model
+{
+    use HasFactory;
+    protected $table = 'will_learns';
+    function courses(){
+        return $this->belongsTo(Courses::class);
+    }
+}
