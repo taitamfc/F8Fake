fc3504993a5406570de895ca999504f80c31656a
thuannguyen <nguyenducthuan0303@gmail.com>
2022-09-12 16:33:51 +0700

    banner_studentmodel

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -5,11 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Course extends Model
+class Banner extends Model
 {
     use HasFactory;
-    protected $table = 'students';
-    function will_learns(){
-        return $this->hasMany(WillLearn::class);
-    }
+    protected $table = 'banners';
 }
