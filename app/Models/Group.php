744425329a07a25dacb6162e9362fef6be90ce64
Ngocvinh2809 <vinhtrann2809@gmail.com>
2022-09-12 15:22:49 +0700

    TrackStep model and Group model

---
+++ app/Models/Group.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    use HasFactory;
+    protected $table = 'groups';
+    function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    function groupRole()
+    {
+        return $this->belongsTo(GroupRole::class);
+    }
+}
---
+++ app/Models/TrackStep.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TrackStep extends Model
+{
+    use HasFactory;
+    protected $table = 'track_steps';
+    function track()
+    {
+        return $this->belongsTo(Track::class);
+    }
+}
