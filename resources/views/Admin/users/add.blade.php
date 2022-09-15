aa35f023ab4b90713664bdb0c0d9f2e09d2ba5c8
Ngocvinh2809 <vinhtrann2809@gmail.com>
2022-09-15 11:30:17 +0700

    UserView

--- resources/views/Admin/users/add.blade.php
+++ resources/views/Admin/users/add.blade.php
@@ -1,11 +1,11 @@
 @extends('Admin.master')
 @section('content')
-
     <div class="page-inner">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item active">
-                    <a href="{{route('users.index')}}"><i class="breadcrumb-icon fa fa-angle-left mr-2"></i>Trang danh sách</a>
+                    <a href="{{ route('users.index') }}"><i class="breadcrumb-icon fa fa-angle-left mr-2"></i>Trang danh
+                        sách</a>
                 </li>
             </ol>
         </nav>
@@ -17,106 +17,121 @@
             <div class="card-deck-xl">
                 <div class="card card-fluid">
                     <div class="card-body">
-                        <form action="{{route('users.store')}}" method="POST" enctype="multipart/form-data">
+                        <form action="{{ route('users.store') }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             <div class="form-group">
                                 <label class="control-label" for="flatpickr01">Tên</label> <input id="flatpickr01"
-                                    type="text" class="form-control" name="name" data-toggle="flatpickr">
+                                    type="text" class="form-control" value="{{ old('name') }}" name="name"
+                                    data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('name') }}</p>
+                                <p style="color:red">{{ $errors->first('name') }}</p>
                             @endif
                             <div class="form-group">
                                 <label class="control-label" for="flatpickr01">Email</label> <input id="flatpickr01"
-                                    type="text" class="form-control" name="email" data-toggle="flatpickr">
+                                    type="text" class="form-control" value="{{ old('email') }}" name="email"
+                                    data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('email') }}</p>
+                                <p style="color:red">{{ $errors->first('email') }}</p>
                             @endif
                             <div class="form-group">
                                 <label class="control-label" for="flatpickr01">Mật Khẩu</label> <input id="flatpickr01"
-                                    type="password" class="form-control" name="password" data-toggle="flatpickr">
+                                    type="password" class="form-control" value="{{ old('password') }}" name="password"
+                                    data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('password') }}</p>
+                                <p style="color:red">{{ $errors->first('password') }}</p>
                             @endif
-                            
+
                             <div class="form-group">
                                 <label class="control-label" for="flatpickr01">User Name</label> <input id="flatpickr01"
-                                    type="text" class="form-control" name="username" data-toggle="flatpickr">
+                                    type="text" class="form-control" value="{{ old('username') }}" name="username"
+                                    data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('username') }}</p>
+                                <p style="color:red">{{ $errors->first('username') }}</p>
                             @endif
                             <div class="form-group">
                                 <label class="control-label" for="flatpickr01">First Name</label> <input id="flatpickr01"
-                                    type="text" class="form-control" name="first_name" data-toggle="flatpickr">
+                                    type="text" class="form-control" value="{{ old('first_name') }}" name="first_name"
+                                    data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('first_name') }}</p>
+                                <p style="color:red">{{ $errors->first('first_name') }}</p>
                             @endif
                             <div class="form-group">
                                 <label class="control-label" for="flatpickr01">Last Name</label> <input id="flatpickr01"
-                                    type="text" class="form-control" name="last_name" data-toggle="flatpickr">
+                                    type="text" class="form-control" value="{{ old('last_name') }}" name="last_name"
+                                    data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('last_name') }}</p>
+                                <p style="color:red">{{ $errors->first('last_name') }}</p>
                             @endif
                             <div class="form-group">
                                 <label class="control-label" for="flatpickr01">Full Name</label> <input id="flatpickr01"
-                                    type="text" class="form-control" name="full_name" data-toggle="flatpickr">
+                                    type="text" class="form-control" value="{{ old('full_name') }}" name="full_name"
+                                    data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('full_name') }}</p>
+                                <p style="color:red">{{ $errors->first('full_name') }}</p>
                             @endif
                             <div class="form-group">
                                 <label class="control-label" for="flatpickr01">Avatar</label> <input id="flatpickr01"
-                                    type="file" class="form-control" name="avatar" data-toggle="flatpickr">
+                                    type="file" class="form-control" value="{{ old('avatar') }}" name="avatar"
+                                    data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('avatar') }}</p>
+                                <p style="color:red">{{ $errors->first('avatar') }}</p>
                             @endif
                             <div class="form-group">
                                 <label class="control-label" for="flatpickr01">Bio</label> <input id="flatpickr01"
-                                    type="text" class="form-control" name="bio" data-toggle="flatpickr">
+                                    type="text" class="form-control" value="{{ old('bio') }}" name="bio"
+                                    data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('bio') }}</p>
+                                <p style="color:red">{{ $errors->first('bio') }}</p>
                             @endif
                             <div class="form-group">
                                 <label class="control-label" for="flatpickr01">Group</label> <input id="flatpickr01"
-                                    type="text" class="form-control" name="group_id" data-toggle="flatpickr">
+                                    type="text" class="form-control" value="{{ old('group_id') }}" name="group_id"
+                                    data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('group_id') }}</p>
+                                <p style="color:red">{{ $errors->first('group_id') }}</p>
                             @endif
                             <div class="form-group">
                                 <label class="control-label" for="flatpickr01">Avatar Url</label> <input id="flatpickr01"
-                                    type="text" class="form-control" name="avatar_url" data-toggle="flatpickr">
+                                    type="text" class="form-control" value="{{ old('avatar_url') }}"
+                                    name="avatar_url" data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('avatar_url') }}</p>
+                                <p style="color:red">{{ $errors->first('avatar_url') }}</p>
                             @endif
                             <div class="form-group">
                                 <label class="control-label" for="flatpickr01">Cover Url</label> <input id="flatpickr01"
-                                    type="text" class="form-control" name="cover_url" data-toggle="flatpickr">
+                                    type="text" class="form-control" value="{{ old('cover_url') }}" name="cover_url"
+                                    data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('cover_url') }}</p>
+                                <p style="color:red">{{ $errors->first('cover_url') }}</p>
                             @endif
                             <div class="form-group">
-                                <label class="control-label" for="flatpickr01">Comment Blocked</label> <input id="flatpickr01"
-                                    type="text" class="form-control" name="is_comment_blocked" data-toggle="flatpickr">
+                                <label class="control-label" for="flatpickr01">Comment Blocked</label> <input
+                                    id="flatpickr01" type="text" class="form-control"
+                                    value="{{ old('is_comment_blocked') }}" name="is_comment_blocked"
+                                    data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('is_comment_blocked') }}</p>
+                                <p style="color:red">{{ $errors->first('is_comment_blocked') }}</p>
                             @endif
                             <div class="form-group">
                                 <label class="control-label" for="flatpickr01">Blocked</label> <input id="flatpickr01"
-                                    type="text" class="form-control" name="is_blocked" data-toggle="flatpickr">
+                                    type="text" class="form-control" value="{{ old('is_blocked') }}"
+                                    name="is_blocked" data-toggle="flatpickr">
                             </div>
                             @if ($errors->any())
-                            <p style="color:red">{{ $errors->first('is_blocked') }}</p>
+                                <p style="color:red">{{ $errors->first('is_blocked') }}</p>
                             @endif
                             <div class="form-group">
                                 <button type="submit" class="btn btn-primary">Thêm</button>
