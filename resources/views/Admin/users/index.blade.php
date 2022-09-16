edee8deaf4b684165938a8c0509716fe0d9e82a2
Ngocvinh2809 <vinhtrann2809@gmail.com>
2022-09-16 14:18:55 +0700

    UserView Try Catch

--- resources/views/Admin/users/index.blade.php
+++ resources/views/Admin/users/index.blade.php
@@ -84,6 +84,13 @@
                             </div>
                             <!-- modalFilterColumns  -->
                             @include('Admin.users.modals.modalFilterColumns')
+                            @if (!count($users))
+                                <p class="text-success">
+                                <div class="alert alert-danger"> <i class="bi bi-x-circle"aria-hidden="true"></i>
+                                    không tìm thấy kết quả.
+                                </div>
+                                </p>
+                            @endif
                             @if (Session::has('success'))
                                 <p class="text-success">
                                 <div class="alert alert-success"> <i class="fa fa-check" aria-hidden="true"></i>
@@ -92,7 +99,7 @@
                             @endif
                             @if (Session::has('error'))
                                 <p class="text-danger">
-                                <div class="alert alert-danger"> <i class="fa fa-check" aria-hidden="true"></i>
+                                <div class="alert alert-danger"> <i class="bi bi-x-circle"aria-hidden="true"></i>
                                     {{ Session::get('error') }}</div>
                                 </p>
                             @endif
