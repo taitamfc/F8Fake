c43412fcf3db3338bee0666be6a2c942ba9d8d9c
Phan Cường <cuongbear@MacBook-Air-cua-Phan.local>
2022-09-29 08:19:05 +0700

    fixbug_view_step,requirement,track

--- app/Http/Controllers/Admin/StepController.php
+++ app/Http/Controllers/Admin/StepController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Exports\StepsExport;
 use App\Http\Requests\StepRequest;
--- app/Http/Controllers/Admin/TrackController.php
+++ app/Http/Controllers/Admin/TrackController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Exports\TracksExport;
 use App\Http\Requests\TrackRequest;
